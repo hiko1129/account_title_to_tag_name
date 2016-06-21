c6b5c579aef6b50d20f66b63a467753209b9ce1f
hiko1129 <wiz69gate4@hotmail.co.jp>
2016-06-21 20:39:51 +0900

    [add]ベース部分を作成
    貸借対照表、損益計算書、キャッシュフロー計算書をハッシュと配列に格納できる状態に

--- csv_parser.rb
+++ csv_parser.rb
@@ -16,10 +16,11 @@ temp.size.times do |i|
   if temp[i][0] == '貸借対照表'
     bs_header = temp[i + 1]
     bs_header.size.times do |j|
-      bs_data[bs_header[j]] = []
+      head = bs_header[j]
+      bs_data[head] = []
       row_num = i + 2
-      while temp[row_num][j] != nil
-        bs_data[bs_header[j]] << temp[row_num][j]
+      until temp[row_num].nil? || temp[row_num][j].nil?
+        bs_data[head] << temp[row_num][j]
         row_num += 1
       end
     end
@@ -27,22 +28,42 @@ temp.size.times do |i|
 
   if temp[i][0] == '損益計算書'
     pl_header = temp[i + 1]
+    pl_header.size.times do |j|
+      head = pl_header[j]
+      pl_data[head] = []
+      row_num = i + 2
+      until temp[row_num].nil? || temp[row_num][j].nil?
+        pl_data[head] << temp[row_num][j]
+        row_num += 1
+      end
+    end
   end
+
 
   if temp[i][0] == 'キャッシュ・フロー計算書'
     cf_header = temp[i + 1]
+    cf_header.size.times do |j|
+      head = cf_header[j]
+      cf_data[head] = []
+      row_num = i + 2
+      until temp[row_num].nil? || temp[row_num][j].nil?
+        cf_data[head] << temp[row_num][j]
+        row_num += 1
+      end
+    end
   end
 
 end
 
-p bs_header
-p pl_header
-p cf_header
-
-bs_data.each do |key, value|
-  puts key
-end
-pp bs_data['標準ラベル（日本語）']
+# p bs_header
+# p pl_header
+# p cf_header
+#
+# bs_data.each do |key, value|
+#   puts key
+# end
+# pp bs_data['標準ラベル（日本語）']
+# pp cf_data['標準ラベル（日本語）']
 # Dir.glob('*.csv').count.times do |i|
 #   p CSV.read('01_d1.csv')
 # end
