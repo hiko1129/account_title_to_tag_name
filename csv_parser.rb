ceb9853f9d1692310e787964fafc13695e15a862
hiko1129 <wiz69gate4@hotmail.co.jp>
2016-06-21 21:23:50 +0900

    [update]保守性の向上
    共通部分をまとめた関数にした

--- csv_parser.rb
+++ csv_parser.rb
@@ -2,68 +2,44 @@ require 'csv'
 require 'pp'
 
 Dir.chdir('./assets')
-puts Dir.pwd
 
-*temp = CSV.read('01_d1.csv')
+temp_data = CSV.read('01_d1.csv')
 bs_header, pl_header, cf_header = nil, nil, nil
+bs_data, pl_data, cf_data = nil, nil, nil
 
-bs_data = {}
-pl_data = {}
-cf_data = {}
-
-temp.size.times do |i|
-
-  if temp[i][0] == '貸借対照表'
-    bs_header = temp[i + 1]
-    bs_header.size.times do |j|
-      head = bs_header[j]
-      bs_data[head] = []
-      row_num = i + 2
-      until temp[row_num].nil? || temp[row_num][j].nil?
-        bs_data[head] << temp[row_num][j]
-        row_num += 1
-      end
+def create_data(input_data, input_num)
+  output_data = {}
+  header = input_data[input_num + 1]
+  header.size.times do |j|
+    head = header[j]
+    output_data[head] = []
+    row_num = input_num + 2
+    until input_data[row_num].nil? || input_data[row_num][j].nil?
+      output_data[head] << input_data[row_num][j]
+      row_num += 1
     end
   end
+  output_data
+end
 
-  if temp[i][0] == '損益計算書'
-    pl_header = temp[i + 1]
-    pl_header.size.times do |j|
-      head = pl_header[j]
-      pl_data[head] = []
-      row_num = i + 2
-      until temp[row_num].nil? || temp[row_num][j].nil?
-        pl_data[head] << temp[row_num][j]
-        row_num += 1
-      end
-    end
+temp_data.size.times do |i|
+
+  comparison_title = temp_data[i][0]
+  if comparison_title == '貸借対照表'
+    bs_data = create_data(temp_data, i)
+  end
+
+  if comparison_title == '損益計算書'
+    pl_data = create_data(temp_data, i)
   end
 
 
-  if temp[i][0] == 'キャッシュ・フロー計算書'
-    cf_header = temp[i + 1]
-    cf_header.size.times do |j|
-      head = cf_header[j]
-      cf_data[head] = []
-      row_num = i + 2
-      until temp[row_num].nil? || temp[row_num][j].nil?
-        cf_data[head] << temp[row_num][j]
-        row_num += 1
-      end
-    end
+  if comparison_title == 'キャッシュ・フロー計算書'
+    cf_data = create_data(temp_data, i)
   end
 
 end
 
-# p bs_header
-# p pl_header
-# p cf_header
-#
-# bs_data.each do |key, value|
-#   puts key
-# end
-# pp bs_data['標準ラベル（日本語）']
-# pp cf_data['標準ラベル（日本語）']
-# Dir.glob('*.csv').count.times do |i|
-#   p CSV.read('01_d1.csv')
-# end
+pl_data.each_key do |key|
+  puts key
+end
