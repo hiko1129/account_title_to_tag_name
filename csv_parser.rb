b2829952a4c60a4b6951b1413799422a1d52b50b
hiko1129 <wiz69gate4@hotmail.co.jp>
2016-06-21 22:46:11 +0900

    [add]標準入力、標準出力追加
    標準入力から勘定科目名を入れることで、標準出力からタグ名を返す。

--- csv_parser.rb
+++ csv_parser.rb
@@ -4,8 +4,8 @@ require 'pp'
 Dir.chdir('./assets')
 
 temp_data = CSV.read('01_d1.csv')
-bs_header, pl_header, cf_header = nil, nil, nil
 bs_data, pl_data, cf_data = nil, nil, nil
+financial_statements = { bs: '貸借対照表', pl: '損益計算書', cf: 'キャッシュ・フロー計算書'}
 
 def create_data(input_data, input_num)
   output_data = {}
@@ -25,21 +25,38 @@ end
 temp_data.size.times do |i|
 
   comparison_title = temp_data[i][0]
-  if comparison_title == '貸借対照表'
+  if comparison_title == financial_statements[:bs]
     bs_data = create_data(temp_data, i)
   end
 
-  if comparison_title == '損益計算書'
+  if comparison_title == financial_statements[:pl]
     pl_data = create_data(temp_data, i)
   end
 
 
-  if comparison_title == 'キャッシュ・フロー計算書'
+  if comparison_title == financial_statements[:cf]
     cf_data = create_data(temp_data, i)
   end
 
 end
 
-pl_data.each_key do |key|
-  puts key
+key_array = []
+bs_data.each_key do |key|
+  key_array << key
 end
+
+puts '勘定科目名を入力してください。'
+input_string = gets.chomp
+def answer(input_data, input_string, key_array)
+  q_list =  input_data[key_array[1]].find_all { |n| n.match(input_string) }
+  a_list = []
+  q_list.each do |i|
+    a_list << input_data[key_array[14]][input_data[key_array[1]].index(i)]
+  end
+  qa_list = Hash[*([q_list, a_list].transpose.flatten)]
+  qa_list
+end
+
+pp answer(bs_data, input_string, key_array)
+pp answer(pl_data, input_string, key_array)
+pp answer(cf_data, input_string, key_array)
